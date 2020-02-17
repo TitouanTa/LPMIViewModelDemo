ed79a0f5e403c2f4e093b9097597ecc0c88291c0
Léonard POTHERAT <l.potherat@planetb.fr>
2020-02-17 11:41:42 +0100

    Wired data to layouts

--- app/src/main/java/org/diiage/lpotherat/poc/lpmiviewmodeldemo/MainActivity.java
+++ app/src/main/java/org/diiage/lpotherat/poc/lpmiviewmodeldemo/MainActivity.java
@@ -7,15 +7,41 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.View;
+
+import org.diiage.lpotherat.poc.lpmiviewmodeldemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //Récupération d'une instance de notre viewmodel
+        //On utilise un ViewModelProvider, jamais directement "new".
+        MainActivityViewModel viewModel =
+        //On indique au provider que this est le StoreOwner
+                new ViewModelProvider(this)
+        //On lui demande ensuite une instance de notre ViewModel
+                        .get(MainActivityViewModel.class);
+
+        //Création de la vue avec DataBindingUtil pour pouvoir récupérer
+        //toutes les informations du binding, notamment le fait que l'on
+        //puisse transmettre le viewmodel
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this
+                        ,R.layout.activity_main);
+
+        //Affectation de this en tant que LifecycleOwner, cela permet au
+        //binding d'observer les LiveData du viewmodel
+        binding.setLifecycleOwner(this);
+
+        //Affectation du viewmodel au binding
+        binding.setViewModel(viewModel);
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
